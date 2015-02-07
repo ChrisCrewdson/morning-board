06c4dd809bbcf01b75e0302989aead4b53f47ff0
Chris Crewdson <chriscrewdson@gmail.com>
2015-02-06 22:29:14 -0800

    fixed uptimerobot and tesla bg

--- jobs/uptimerobot.rb
+++ jobs/uptimerobot.rb
@@ -2,19 +2,18 @@ require 'uptimerobot'
 
 apiKey = ENV['UPTIMEROBOT_APIKEY']
 
-SCHEDULER.every '1m', :first_in => 0 do |job|
+SCHEDULER.every '5m', :first_in => 0 do |job|
   client = UptimeRobot::Client.new(apiKey: apiKey)
 
-  monitors = client.getMonitors['monitors']['monitor']
+  raw_monitors = client.getMonitors['monitors']['monitor']
 
-  send_event('uptimerobot', {
-    friendlyname0: monitors[0]['friendlyname'],
-    status0: 'S' << monitors[0]['status'],
-    friendlyname1: monitors[1]['friendlyname'],
-    status1: 'S' << monitors[1]['status'],
-    friendlyname2: monitors[2]['friendlyname'],
-    status2: 'S' << monitors[2]['status'],
-    friendlyname3: monitors[3]['friendlyname'],
-    status3: 'S' << monitors[3]['status'],
-  })
+  monitors = raw_monitors.map { |monitor| 
+    { 
+      friendlyname: monitor['friendlyname'], 
+      status: 'S' << monitor['status']
+    }
+  }
+
+  send_event('uptimerobot', { monitors: monitors } )
 end
+
