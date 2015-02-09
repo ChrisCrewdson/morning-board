8b483904f211ac146a601cf18680a9042ad6619e
Chris Crewdson <chriscrewdson@gmail.com>
2015-02-08 17:34:30 -0800

    hide xively api key

--- jobs/xively.rb
+++ jobs/xively.rb
@@ -1,10 +1,12 @@
 require 'httparty'
 
+XIVELY_APIKEY = ENV['XIVELY_APIKEY']
+
 SCHEDULER.every '1m', :first_in => 0 do |job|
   begin
     # Hit Xively JSON endpoint
     feedResult = HTTParty.get("https://api.xively.com/v2/feeds/1523709162",
-                { :headers => { 'X-ApiKey' => "js5Sab5uZwmSsE2Ou1zWXRalPdXzrttAfaWmhtYA6FFjgBSt", "Content-Type" => 'application/json'}})
+                { :headers => { 'X-ApiKey' => XIVELY_APIKEY, "Content-Type" => 'application/json'}})
 
     # Get the current value
     current_value = feedResult['datastreams'][0]["current_value"]
